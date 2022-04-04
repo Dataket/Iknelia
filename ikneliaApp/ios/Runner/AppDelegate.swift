099116ffb3da166180497a625c6ad0a0d1eaa81c
ysaucedo <55114540+ysaucedo@users.noreply.github.com>
2022-04-03 23:25:17 -0500

    Loading....

--- ikneliaApp/ios/Runner/AppDelegate.swift
+++ ikneliaApp/ios/Runner/AppDelegate.swift
@@ -1,13 +1,146 @@
 import UIKit
 import Flutter
+import AVFoundation
 
 @UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate {
+@objc class AppDelegate: FlutterAppDelegate, AVAudioRecorderDelegate {
+    
+  var recorder: AVAudioRecorder!
+  var soundFileURL: URL!
+    var isRecording: Bool = false
+    
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+      let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+      
+      let METHOD_CHANNEL_NAME = "battery"
+      let batteryChannel = FlutterMethodChannel(
+        name: METHOD_CHANNEL_NAME,
+        binaryMessenger: controller.binaryMessenger)
+      
+      batteryChannel.setMethodCallHandler({
+          (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+          switch call.method {
+          case "getBatteryLevel":
+                guard let args = call.arguments as? [String: String] else {return}
+                let action = args["action"]!
+              if !self.isRecording {
+                  self.isRecording = true
+                  result("\(action) \(self.reciveBatteryLevel())")
+              } else {
+                  self.isRecording = false
+                  self.recorder.stop()
+                  result("Stop")
+              }
+          default:
+              result(FlutterMethodNotImplemented)
+          }
+      })
+      
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+    
+    private func reciveBatteryLevel() -> Int {
+        let device = UIDevice.current
+        device.isBatteryMonitoringEnabled = true
+        
+        recordWithPermission(true)
+        
+        if device.batteryState == UIDevice.BatteryState.unknown {
+            return -1
+        } else {
+            return Int(device.batteryLevel * 100)
+        }
+    }
+    
+    func recordWithPermission(_ setup: Bool) {
+        
+        AVAudioSession.sharedInstance().requestRecordPermission {
+            [unowned self] granted in
+            if granted {
+                
+                DispatchQueue.main.async {
+                    print("Permission to record granted")
+                    if setup {
+                        self.setupRecorder()
+                    }
+                    self.recorder.record()
+                }
+            } else {
+                print("Permission to record not granted")
+            }
+        }
+        
+        if AVAudioSession.sharedInstance().recordPermission == AVAudioSession.RecordPermission.denied {
+            print("permission denied")
+        }
+    }
+    
+    
+    func setupRecorder() {
+        
+        let format = DateFormatter()
+        format.dateFormat="yyyy-MM-dd-HH-mm-ss"
+        //let currentFileName = "recording-\(format.string(from: Date())).m4a"
+        let currentFileName = "recording-\(format.string(from: Date())).wav"
+        print(currentFileName)
+        
+        let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        self.soundFileURL = documentsDirectory.appendingPathComponent(currentFileName)
+        print("writing to soundfile url: '\(soundFileURL!)'")
+        
+        if FileManager.default.fileExists(atPath: soundFileURL.absoluteString) {
+            // probably won't happen. want to do something about it?
+            print("soundfile \(soundFileURL.absoluteString) exists")
+        }
+
+        /*
+        let recordSettings: [String: Any] = [
+            AVFormatIDKey: kAudioFormatAppleLossless,
+            AVEncoderAudioQualityKey: AVAudioQuality.max.rawValue,
+            AVEncoderBitRateKey: 32000,
+            AVNumberOfChannelsKey: 2,
+            AVSampleRateKey: 44100.0
+        ]
+        */
+        let recordSettings: [String : Any] = [AVFormatIDKey: Int(kAudioFormatLinearPCM),
+                                              AVSampleRateKey: 44100.0,
+                                              AVNumberOfChannelsKey: 1,
+                                              AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue ]
+        
+        
+        do {
+            recorder = try AVAudioRecorder(url: soundFileURL, settings: recordSettings)
+            recorder.delegate = self
+            recorder.isMeteringEnabled = true
+            recorder.prepareToRecord() // creates/overwrites the file at soundFileURL
+        } catch {
+            recorder = nil
+            print(error.localizedDescription)
+        }
+        
+    }
+    
+}
+
+// MARK: AVAudioPlayerDelegate
+extension AppDelegate: AVAudioPlayerDelegate {
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        print("\(#function)")
+        
+        print("finished playing \(flag)")
+    }
+    
+    
+    func audioPlayerDecodeErrorDidOccur(_ player: AVAudioPlayer, error: Error?) {
+        print("\(#function)")
+        
+        if let e = error {
+            print("\(e.localizedDescription)")
+        }
+        
+    }
 }
